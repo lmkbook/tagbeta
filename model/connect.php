7250934b7c1d86580bbaf222d1bea3abaa1a7102
lmkbook <167584338+lmkbook@users.noreply.github.com>
2024-10-24 13:16:57 -0500

    Add files via upload

--- model/connect.php
+++ model/connect.php
@@ -6,12 +6,34 @@
     <title>CONEXION</title>
 </head>
 <body>
-    <?php
-        try{
-            $sqli = new PDO("mysql:host=127.0.0.1; dbname=tagmypet", 'root', '');
-            $sqli->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $e){
-            die("Error con la base de datos: " . $e->getMessage());
+    <?php  
+        class Connect{
+            private static $instance = null;
+            private $connection;
+            private $host = '127.0.0.1';
+            private $user = 'root';
+            private $pass = '';
+            private $db = 'tagmypet';
+            private function __construct(){
+                try{
+                    $this->connection = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pass, [
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                        PDO::ATTR_EMULATE_PREPARES => false,
+                        PDO::ATTR_PERSISTENT => false,
+                    ]);
+                }catch(PDOException $e){
+                    error_log("Ocurrio un error con la base de datos: " . $e->getMessage());
+                    die("Ocurrio un error con la conexion");
+                }
+            }
+            public static function ObtainInstance(){
+                if(!self::$instance){
+                    self::$instance = new Connect();
+                }
+                return self::$instance->connection;
+            }
         }
     ?>
 </body>
