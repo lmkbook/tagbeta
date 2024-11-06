07892c66ded532473b33fbbfe68d5a75a368cc4e
Crisweisk <119748845+Crisweisk@users.noreply.github.com>
2024-11-06 16:48:04 -0500

    Delete javascript directory

--- javascript/footer.js
+++
@@ -1,9 +0,0 @@
-// footer.js
-window.onload = function() {
-    fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.body.insertAdjacentHTML('beforeend', data);
-    })
-    .catch(error => console.log('Error al cargar el footer:', error));
-}
